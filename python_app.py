c66f608a62cf471ad314025301287f69d942ad69
KarLOL82 <klinfeldt@gmail.com>
2023-01-05 20:10:18 -0800

    working binary search

--- python_app.py
+++ python_app.py
@@ -15,3 +15,12 @@ def binary_search(list, element):
             return middle
         if element < list[middle]:
             end = middle -1
+        else:
+            start = middle + 1
+    
+    return -1
+
+my_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+target = 13
+
+binary_search(my_list, target)
